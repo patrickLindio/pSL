fc79f4b991b12848bd26edf9f269e6ba861a1380
patrickLindio <lindiopat@gmail.com>
2025-01-04 18:34:23 +0800

    Create tasksheet-identifier.java

---
+++ tasksheet-identifier.java
@@ -0,0 +1,11 @@
+public class tasksheet-identifier {
+  public static void main (String [] args ) {
+    char H = 'H';
+    int three = 3;
+    int one = 1;
+    int zero = 0;
+    float twoPointZero = 2.0
+    boolean isTrue = true;
+    String output = "" + H + three + one + one + zero + "w" + zero + "r" + one + "d" + twoPointZero + "" +isTRue;
+    System.out.println(output); }
+}
